ffa1867aff109385c11b4983684d8bec15d249f4
Md Rashid <118841846+rashidrehan12@users.noreply.github.com>
2025-07-22 15:41:02 +0530

    show ceated hisaab file

--- app.js
+++ app.js
@@ -16,4 +16,58 @@ app.get('/', (req, res) => {
     });  
 });
 
+app.get("/create", function (req, res) {
+    res.render("create");
+});
+
+
+app.get("/edit/:filename", function (req, res) {
+  fs.readFile(`./hisaab/${req.params.filename}`, "utf-8", function (err, filedata) {
+    if (err) return res.status(500).send(err);
+    res.render("edit", { filedata, filename: req.params.filename });
+  });
+});
+
+// app.post("/createhisaab", function (req, res) {
+
+//   var currentDate = new Date();
+//   var date = `${currentDate.getDate()}-${currentDate.getMonth()+1}-${currentDate.getFullYear()}`
+
+//   fs.writeFile(`./hisaab/${date}.txt`, req.body.content, function (err) {
+//     if (err) return res.status(500).send(err);
+//     res.redirect("/");
+//   });
+// });
+
+app.post("/createhisaab", function (req, res) {
+    var currentDate = new Date();
+    var date = `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}`;
+    var counter = 0;
+    var fileName = `${date}.txt`;
+
+    // Helper function to get a unique filename
+    function getUniqueFileName(callback) {
+        var filePath = `./hisaab/${fileName}`;
+        fs.stat(filePath, function (err) {
+            if (err) {
+                // File does not exist, safe to use this name
+                callback(filePath);
+            } else {
+                // File exists, increment counter and try a new name
+                counter++;
+                fileName = `${date}-${counter}.txt`;
+                getUniqueFileName(callback);
+            }
+        });
+    }
+
+    getUniqueFileName(function (uniquePath) {
+        fs.writeFile(uniquePath, req.body.content, function (err) {
+            if (err) return res.status(500).send(err);
+            res.redirect("/");
+        });
+    });
+});
+
+
 app.listen(3000);
